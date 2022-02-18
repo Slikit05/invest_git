435dee2121db6b65ace3264b23083901166a1c1f
Slikit05 <slikit@mail.ru>
2022-02-18 18:04:20 +0300

    18.02.2022

---
+++ docs/js/groups.js
@@ -0,0 +1,118 @@
+// Группы объектов
+var groups = {
+  1: {
+    name: "Краснодар",
+    style: "islands#redIcon",
+    items: [{
+        center: [45.06348671474117, 38.97236140740968],
+        // name: "г. Челябинск, Свердловский тракт, 7/1",
+        // dateOne: "Пн-cб 9:00—21:00",
+        // dateTwo: "Вс 9:00—20:00",
+        // tel: [
+        //   'Автосервис: <span> +7 (351) 239-15-52</span>',
+        //   'Автозапчасти: <span> +7 (351) 239-15-52</span>',
+        // ],
+        // img: [
+        //   'http://localhost:3000/img/Renault_Logosm.png',
+        //   'http://localhost:3000/img/Suzuki_logosm2.png',
+        // ]
+      },
+      {
+        center: [45.06548690451672, 38.982781552917494],
+        // name: "г. Челябинск, Свердловский тракт, 7/1",
+        // dateOne: "Пн-cб 9:00—21:00",
+        // dateTwo: "Вс 9:00—20:00",
+        // tel: [
+        //   'Автосервис: <span> +7 (351) 239-15-52</span>',
+        //   'Автозапчасти: <span> +7 (351) 239-15-52</span>',
+        // ],
+        // img: [
+        //   'http://localhost:3000/img/Renault_Logosm.png',
+        //   'http://localhost:3000/img/Suzuki_logosm2.png',
+        // ]
+      },
+      {
+        center: [45.06503831196623, 39.00621333087158],
+        // name: "г. Челябинск, Свердловский тракт, 7/1",
+        // dateOne: "Пн-cб 9:00—21:00",
+        // dateTwo: "Вс 9:00—20:00",
+        // tel: [
+        //   'Автосервис: <span> +7 (351) 239-15-52</span>',
+        //   'Автозапчасти: <span> +7 (351) 239-15-52</span>',
+        // ],
+        // img: [
+        //   'http://localhost:3000/img/Renault_Logosm.png',
+        //   'http://localhost:3000/img/Suzuki_logosm2.png',
+        // ]
+      }
+    ]
+  },
+  2: {
+    name: "Москва",
+    items: [{
+        center: [50.426472, 30.563022],
+        // name: "г. Челябинск, Свердловский тракт, 7/1",
+        // dateOne: "Пн-cб 9:00—21:00",
+        // dateTwo: "Вс 9:00—20:00",
+        // tel: [
+        //   'Автосервис: <span> +7 (351) 239-15-52</span>',
+        //   'Автозапчасти: <span> +7 (351) 239-15-52</span>',
+        // ],
+        // img: [
+        //   'http://localhost:3000/img/Renault_Logosm.png',
+        //   'http://localhost:3000/img/Suzuki_logosm2.png',
+        // ]
+      },
+      {
+        center: [45.06166188109295, 38.9622065],
+        // name: "г. Челябинск, Свердловский тракт, 7/1",
+        // dateOne: "Пн-cб 9:00—21:00",
+        // dateTwo: "Вс 9:00—20:00",
+        // tel: [
+        //   'Автосервис: <span> +7 (351) 239-15-52</span>',
+        //   'Автозапчасти: <span> +7 (351) 239-15-52</span>',
+        // ],
+        // img: [
+        //   'http://localhost:3000/img/Renault_Logosm.png',
+        //   'http://localhost:3000/img/Suzuki_logosm2.png',
+        // ]
+      },
+      {
+        center: [50.454433, 30.529874],
+        name: "г. Челябинск, Свердловский тракт, 7/1",
+        dateOne: "Пн-cб 9:00—21:00",
+        dateTwo: "Вс 9:00—20:00",
+        tel: [
+          'Автосервис: <span> +7 (351) 239-15-52</span>',
+          'Автозапчасти: <span> +7 (351) 239-15-52</span>',
+        ],
+        img: [
+          'http://localhost:3000/img/Renault_Logosm.png',
+          'http://localhost:3000/img/Suzuki_logosm2.png',
+        ]
+      }
+    ]
+  },
+};
+
+var city = {
+  name: "Города",
+  style: "islands#redIcon",
+  items: {
+    1: {
+      center: [45.06166188109295, 38.9622065],
+      name: "",
+      zoom: 12,
+    },
+    2: {
+      center: [55.584222181163646, 37.38552449999999],
+      name: "",
+      zoom: 12,
+    },
+    3: {
+      center: [45.06503831196623, 39.00621333087158],
+      name: "",
+      zoom: 12,
+    }
+  }
+};
